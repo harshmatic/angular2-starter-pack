5e403d5f89f7971879a50cc429622a4e3228ca1a
SagarShelar <sagar.shelar@eternussolutions.com>
2017-04-20 17:23:33 +0530

    added route based authoriazation

--- src/modules/employee/employee.routing.ts
+++ src/modules/employee/employee.routing.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { EmployeeComponent } from './components/employee.component';
-
+import { AuthGuard } from '../../app/core/index'
 
 const routes: Routes = [
   {
     path: '',
-    component: EmployeeComponent
+    component: EmployeeComponent,
+    data: {
+      permissions: ['EMPLOYEE_READ']
+    },
+     canActivate: [AuthGuard],
   }
 ];
 
