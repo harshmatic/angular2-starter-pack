973b68a98f014bc3405a7c6561e3eeff316b87dd
Pranoti.Vaidya <pranoti.vaidya@eternussolitions.com>
2017-04-25 11:13:47 +0530

    Changes done

--- src/modules/area/components/area.component.ts
+++ src/modules/area/components/area.component.ts
@@ -32,6 +32,14 @@ export class AreaComponent  implements OnInit {
 	"value":"test after patch"
   });
     this.store.dispatch({ type: AREA_ACTIONS.GET_LIST });
+     // //Create
+   // this.store.dispatch({ type: AREA_ACTIONS.ADD,payload:this.areaObj });
+    // //Delete
+   // this.store.dispatch({ type: AREA_ACTIONS.ADD });
+    // //Update (PUT)
+    //this.store.dispatch({ type: AREA_ACTIONS.UPDATE,payload:{id:"56c385ae-ce46-41d4-b7fe-08df9aef9579",updates:this.areaObj}});
+    // //Update (PATCH)
+    // this.store.dispatch({ type: AREA_ACTIONS.UPDATE,payload:{id:"88f50d32-bb51-4835-90e9-1b02c8109ab2",updates:this.patchReq}});
     this.store.select('area').subscribe((res:any) => {
        this.area = res;
      });
