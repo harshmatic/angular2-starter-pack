d9f8dac31c70d0b444aafe8287084f3ae249d859
SagarShelar <sagar.shelar@eternussolutions.com>
2017-04-26 14:38:56 +0530

    added angular map

--- src/modules/dashboard/components/dashboard.component.ts
+++ src/modules/dashboard/components/dashboard.component.ts
@@ -7,18 +7,19 @@ import { DASHBOARD_ACTIONS } from '../store/dashboard.actions';
   moduleId: module.id,
   selector: 'app-dashboard',
   templateUrl: 'dashboard.component.html',
+  styleUrls:['dashboard.component.css']
 })
 export class DashboardComponent  implements OnInit {
   dashboard:Dashboard[];
+  lat: number = 51.678418;
+  lng: number = 7.809007;
   constructor(private store: Store<Dashboard>){}
   ngOnInit() {
     this.store.dispatch({ type: DASHBOARD_ACTIONS.GET_DASHBOARD_LIST });
-     this.store.dispatch({ type: DASHBOARD_ACTIONS.ADD_DASHBOARD });
-     
-     this.store.select('dashboard').subscribe((res:any) => {
-      
+    this.store.dispatch({ type: DASHBOARD_ACTIONS.ADD_DASHBOARD });
+    this.store.select('dashboard').subscribe((res:any) => {
        this.dashboard = res;
-     });
+    });
      
   }
 }
