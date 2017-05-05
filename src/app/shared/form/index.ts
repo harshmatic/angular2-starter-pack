426f492f32896fd608489e1aac7fb007ca972908
harshmatic <harshit.jyoti@eternussolutions.com>
2017-05-05 17:57:11 +0530

    UPGRADE TO ANGULAR 4

--- src/app/shared/form/index.ts
+++ src/app/shared/form/index.ts
@@ -1,7 +1,7 @@
-import { EsplForm } from './form';
-import { EsplFormError } from './form-error';
-import { EsplFormGroup } from './form-group';
-import { EsplLabel } from './label';
-import { EsplInput } from './input';
+// import { EsplForm } from './form';
+// import { EsplFormError } from './form-error';
+// import { EsplFormGroup } from './form-group';
+// import { EsplLabel } from './label';
+// import { EsplInput } from './input';
 
-export { EsplForm, EsplFormError, EsplFormGroup, EsplLabel, EsplInput };
+// export { EsplForm, EsplFormError, EsplFormGroup, EsplLabel, EsplInput };
