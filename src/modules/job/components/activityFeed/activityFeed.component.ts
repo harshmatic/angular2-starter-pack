c5c4440b3f2213a402026aa8a6256b80a7e0f55e
harshmatic <harshit.jyoti@eternussolutions.com>
2017-05-09 12:27:18 +0530

    package json

--- src/modules/job/components/activityFeed/activityFeed.component.ts
+++ src/modules/job/components/activityFeed/activityFeed.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit,AfterViewInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
 import { ACTIVITY_ACTIONS } from '../../../activity/store/activity.actions';
+import { ActivatedRoute, Router } from '@angular/router';
 declare var $:any;
 @Component({
   moduleId: module.id,
@@ -12,7 +13,7 @@ export class ActivityFeedComponent  implements OnInit {
   activities:any[]=[];
   asyncActivities:Observable<any>
   showTab:boolean=true;
-  constructor(private store: Store<any>){}
+  constructor(private store: Store<any>, private router: Router){}
 
   ngOnInit() {
     this.store.dispatch({ type: ACTIVITY_ACTIONS.GET_LIST });
@@ -21,5 +22,8 @@ export class ActivityFeedComponent  implements OnInit {
        this.activities = res;
     });
      
+  }
+  onOBClick(id: any) {
+    this.router.navigate(['/jobs/jobEdit'], { queryParams: { OccurenceBookID: id } });
   }
 }
