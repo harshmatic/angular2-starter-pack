429fc8e8febb61997173040467c770d04effbb73
SagarShelar <sagar.shelar@eternussolutions.com>
2017-05-09 16:13:49 +0530

    added activity feed page for officer

--- src/modules/officers/components/activityFeed/activityFeed.component.ts
+++ src/modules/officers/components/activityFeed/activityFeed.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit,AfterViewInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs/Observable';
 import { Employee } from '../../../employee/store/employee.model';
-import { EMPLOYEE_ACTIONS } from '../../../employee/store/employee.actions';
+import { ACTIVITY_ACTIONS } from '../../../activity/store/activity.actions';
+import { ActivatedRoute, Router } from '@angular/router';
 declare var $:any;
 @Component({
   moduleId: module.id,
@@ -10,17 +11,40 @@ declare var $:any;
   templateUrl: 'activityFeed.component.html',
 })
 export class ActivityFeedComponent  implements OnInit {
-  officers:any[]=[];
-  asyncOfficer:Observable<any>
+   activities:any[]=[];
+  asyncActivities:Observable<any>
   showTab:boolean=true;
-  constructor(private store: Store<Employee>){}
+  pageNum:number = 1;
+   stopScroll = false;
+  constructor(private store: Store<any>, private router: Router){}
 
   ngOnInit() {
-    // this.store.dispatch({ type: EMPLOYEE_ACTIONS.GET_LIST });
-    // this.asyncOfficer= this.store.select('employee')
-    // this.asyncOfficer.subscribe((res:any) => {
-    //    this.officers = res;
-    // });
+    this.activities=[];
+    this.getActivities();
+    this.asyncActivities= this.store.select('activity')
+
+    this.asyncActivities.subscribe((res: any) => {
+      for (let i = res.length - 1; i >= 0; i--) {
+        this.activities.push(res[i]);
+      }
+      if (res.length > 0) {
+        this.pageNum++;
+        this.stopScroll = false;
+      } else {
+        this.stopScroll = true;
+      }
+    })
      
+  }
+  getActivities(){
+     if (!this.stopScroll && this.pageNum > 0) {
+     this.store.dispatch({
+      type: ACTIVITY_ACTIONS.GET_LIST,
+      payload: { pageNum: this.pageNum, pageSize: 5 }
+    });
+  }
+  }
+  onOBClick(id: any) {
+    this.router.navigate(['/jobs/jobEdit'], { queryParams: { OccurenceBookID: id } });
   }
 }
